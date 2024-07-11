2c76ed53f818be066ed6d3bd759aa555e110af3c
Amos <adeyemoamos33@gmail.com>
2024-07-11 01:57:00 -0700

    home_Page

--- src/app.js
+++ src/app.js
@@ -26,3 +26,74 @@ const openWork = e => {
 };
 
 howItWorksContainer.addEventListener('click', openWork);
+
+const slider = () => {
+  const clients = document.querySelectorAll('.client');
+  const dotContainer = document.querySelector('.dots');
+
+  let currentSlide = 0;
+  const maxSlide = clients.length;
+
+  const createDots = () => {
+    clients.forEach((_, i) => {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `
+        <button class="dots__dot" data-slide="${i}"></button>
+        `
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = function (slide) {
+    clients.forEach(
+      (s, i) => (
+        (s.style.transform = `translateX(${100 * (i - slide)}px)`), 0, 0
+      )
+    );
+    // document.querySelector('.view').style.transform = `translate3d(${
+    //   300 * slide
+    // })px`;
+    // clients.forEach(
+    //   (s, i) => (s.style.transform = `translate3d(${100 * (i - slide)}px)`, 0, 0)
+    // );
+  };
+  setInterval(() => {
+    if (currentSlide === maxSlide - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+
+    goToSlide(currentSlide);
+    activateDot(currentSlide);
+  }, 5000);
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+
+slider();
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
+      gray: '#fafafa',
       white: '#ffffff',
       orange: {
         100: '#ffedd5',
