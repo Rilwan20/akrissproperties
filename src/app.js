49ec2f81fed7cbc0330bcc02776a59d37992c05e
Amos <adeyemoamos33@gmail.com>
2024-07-12 12:04:33 -0700

    scroll

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const howItWork = document.querySelectorAll('.a');
 const howItWorksContainer = document.querySelector('.how_it_work_container');
 
+// How it works functionality
 const openWork = e => {
   const selectedWorkParent = e.target.closest('.how_it_work');
 
@@ -27,6 +28,8 @@ const openWork = e => {
 
 howItWorksContainer.addEventListener('click', openWork);
 
+// Navigation functionality
+
 const NavBar = document.querySelector('#bars');
 
 const Navs = document.querySelector('#navs');
@@ -41,3 +44,43 @@ const toogleNavs = () => {
 };
 
 LoadEvenListerners();
+
+// Revealing Section on Scroll
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// Scroll onto view
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  } else {
+    const select = e.target.closest('.text-xs').getAttribute('href');
+
+    window.location.href = `${select}`;
+  }
+});
--- src/team.js
+++ src/team.js
@@ -25,3 +25,38 @@ const toogleNavs = () => {
 };
 
 LoadEvenListerners();
+
+// Revealing Section on Scroll
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// Scroll onto view
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
